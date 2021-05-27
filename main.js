f7f4be2049564e64893a4b8f72448a2fe34c7cf3
Formacion <formacion@FTAULAS.ORG>
2021-05-27 12:58:01 +0200

    incrementar y descrementar

---
+++ main.js
@@ -0,0 +1,20 @@
+let puntaje = 0;
+console.log(puntaje);
+
+console.log(puntaje ++);
+
+console.log(puntaje);
+
+console.log(++puntaje);
+
+console.log(puntaje+=5);
+console.log("__________")
+
+
+let volumen = 10;
+
+console.log(volumen--);
+
+console.log(--volumen);
+
+console.log(volumen-=3);
